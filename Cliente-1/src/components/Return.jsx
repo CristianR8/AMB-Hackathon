f92540f27911f7d7ee518b25dc740d79a52e4b85
CristianR8 <cristiancamiloreyrueda@gmail.com>
2023-11-24 11:05:31 -0500

    v3

--- Cliente-1/src/components/Return.jsx
+++ Cliente-1/src/components/Return.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { IoIosReturnLeft } from "react-icons/io";
+import { MdDownload } from "react-icons/md";
 
 const Return = ({ to, text }) => {
   return (
@@ -8,7 +9,7 @@ const Return = ({ to, text }) => {
       to={to}
       className="px-8 py-4 rounded-xl fixed bottom-4 right-8 m-4 text-bold text-lg z-0 bg-neutral-950 hover:scale-110 hover:bg-neutral-50 hover:text-neutral-950 hover:border-neutral-950 text-neutral-50 transition-all duration-200 shadow-2xl"
     >
-        Volver
+        Return
       <IoIosReturnLeft />
 
     </Link>
--- Cliente-1/src/components/Segmentation.jsx
+++ Cliente-1/src/components/Segmentation.jsx
@@ -2,26 +2,43 @@ import React from "react";
 import Return from "./Return";
 import ParticlesBackground from "./ParticlesBackground";
 import { useLocation } from "react-router-dom";
+import { MdDownload } from "react-icons/md";
 import segmentation from "../assets/img/segmentation.png";
 
 const Segmentation = () => {
   const location = useLocation();
-  const { originalImage } = location.state || {};
-  const { segmentedImage } = location.state || {};
+  const { originalImage, segmentedImage } = location.state || {};
   return (
     <div className="p-4">
       <div className="flex justify-center items-center space-x-4">
-        {originalImage && (
+        {segmentation && (
           <>
+          <div className="relative">
+              <button
+                className=" absolute bottom-52 left-80 flex items-center justify-center rounded-xl bg-neutral-950 hover:bg-neutral-50 hover:text-neutral-950 hover:scale-110 text-neutral-50 py-2 px-4 mt-4 text-lg font-bold transition-all duration-200 shadow-2xl"
+                onClick={() => {
+                  const link = document.createElement("a");
+                  link.href = segmentedImage;
+                  link.download = {segmentedImage};
+                  document.body.appendChild(link);
+                  link.click();
+                  document.body.removeChild(link);
+                }}
+              >
+                Download<MdDownload className="ml-2" />
+              </button>
+            </div>
             <img src={originalImage} className="mx-auto right--64 h-96 w-96" />
             <img src={segmentedImage} className="mx-auto right--64 h-96 w-96" />
+            
           </>
         )}
-        <Return to="/" />
       </div>
       <p className="text-center text-2xl mt-2">
         This is the result of the segmentation process!
       </p>
+
+      <Return to="/" />
     </div>
   );
 };
